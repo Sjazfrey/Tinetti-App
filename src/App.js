60a0db9113ec1fdacbc18d36600ae7a20f0c0e89
Shirley Kaffrey <jazfrey@gmail.com>
2020-08-30 13:56:51 -0600

    after change to bootstrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
-
-
 import Home from './components/Home';
 // import Result from './components/Result';
 import Test from './components/Test';
